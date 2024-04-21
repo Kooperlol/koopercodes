31cd16aa983a334013a79c951becbd31407b131a
Kooper <74805059+Kooperlol@users.noreply.github.com>
2024-04-21 13:46:30 -0500

    Finish

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import KooperParticles from "@/components/particles";
-import { Curve } from "@/components/transition";
 import {
   Box,
   Button,
@@ -136,112 +135,110 @@ const ContactPage = () => {
   };
 
   return (
-    <Curve>
-      <div className="flex items-center justify-center">
-        <Image
-          className="top-0 left-0 absolute object-cover w-screen h-screen"
-          priority
-          draggable={false}
-          src="/images/banner.svg"
-          fill
-          alt={"Banner Image"}
-        />
-        {videos[Math.floor(Math.random() * videos.length)]}
+    <div className="flex items-center justify-center">
+      <Image
+        className="top-0 left-0 absolute object-cover w-screen h-screen"
+        priority
+        draggable={false}
+        src="/images/banner.svg"
+        fill
+        alt={"Banner Image"}
+      />
+      {videos[Math.floor(Math.random() * videos.length)]}
 
-        <KooperParticles />
-        <div className="container z-10 items-center justify-center flex h-screen py-12">
-          <Stack
-            bg={"gray.50"}
-            rounded={"xl"}
-            p={{ base: 4, sm: 6, md: 8 }}
-            spacing={{ base: 8 }}
-            className="w-2/3 max-w-lg"
-          >
-            <Stack spacing={4}>
-              <Heading
-                color={"gray.800"}
-                lineHeight={1.1}
-                fontSize={{ base: "2xl", sm: "3xl", md: "4xl" }}
-              >
-                Contact Kooper
-                <Text
-                  as={"span"}
-                  bgGradient="linear(to-r, blue.400,blue.600)"
-                  bgClip="text"
-                >
-                  !
-                </Text>
-              </Heading>
-              <Text color={"gray.500"} fontSize={{ base: "sm", sm: "md" }}>
-                I'm always looking for new opportunities to collaborate with
-                others. Whether you're looking for a new website, a new feature,
-                or just want to chat, I'm always open to new ideas.
-              </Text>
-            </Stack>
-            <Box as={"form"} ref={formRef} id="contact" onSubmit={handleSubmit}>
-              <Stack spacing={4}>
-                <Input
-                  id="firstname"
-                  placeholder="First Name"
-                  bg={"gray.100"}
-                  border={0}
-                  color={"gray.500"}
-                  _placeholder={{
-                    color: "gray.500",
-                  }}
-                />
-                <Input
-                  id="lastname"
-                  placeholder="Last Name"
-                  bg={"gray.100"}
-                  border={0}
-                  color={"gray.500"}
-                  _placeholder={{
-                    color: "gray.500",
-                  }}
-                />
-                <Input
-                  id="email"
-                  placeholder="Email"
-                  bg={"gray.100"}
-                  border={0}
-                  type="email"
-                  color={"gray.500"}
-                  _placeholder={{
-                    color: "gray.500",
-                  }}
-                />
-                <Textarea
-                  id="message"
-                  placeholder="Message"
-                  bg={"gray.100"}
-                  border={0}
-                  color={"gray.500"}
-                  _placeholder={{
-                    color: "gray.500",
-                  }}
-                />
-              </Stack>
-              <Button
-                fontFamily={"heading"}
-                mt={8}
-                w={"full"}
+      <KooperParticles />
+      <div className="container z-10 items-center justify-center flex h-screen py-12">
+        <Stack
+          bg={"gray.50"}
+          rounded={"xl"}
+          p={{ base: 4, sm: 6, md: 8 }}
+          spacing={{ base: 8 }}
+          className="w-2/3 max-w-lg"
+        >
+          <Stack spacing={4}>
+            <Heading
+              color={"gray.800"}
+              lineHeight={1.1}
+              fontSize={{ base: "2xl", sm: "3xl", md: "4xl" }}
+            >
+              Contact Kooper
+              <Text
+                as={"span"}
                 bgGradient="linear(to-r, blue.400,blue.600)"
-                color={"white"}
-                type="submit"
-                _hover={{
-                  bgGradient: "linear(to-r, blue.400,blue.600)",
-                  boxShadow: "xl",
-                }}
+                bgClip="text"
               >
-                Submit
-              </Button>
-            </Box>
-            form
+                !
+              </Text>
+            </Heading>
+            <Text color={"gray.500"} fontSize={{ base: "sm", sm: "md" }}>
+              I'm always looking for new opportunities to collaborate with
+              others. Whether you're looking for a new website, a new feature,
+              or just want to chat, I'm always open to new ideas.
+            </Text>
           </Stack>
-        </div>
+          <Box as={"form"} ref={formRef} id="contact" onSubmit={handleSubmit}>
+            <Stack spacing={4}>
+              <Input
+                id="firstname"
+                placeholder="First Name"
+                bg={"gray.100"}
+                border={0}
+                color={"gray.500"}
+                _placeholder={{
+                  color: "gray.500",
+                }}
+              />
+              <Input
+                id="lastname"
+                placeholder="Last Name"
+                bg={"gray.100"}
+                border={0}
+                color={"gray.500"}
+                _placeholder={{
+                  color: "gray.500",
+                }}
+              />
+              <Input
+                id="email"
+                placeholder="Email"
+                bg={"gray.100"}
+                border={0}
+                type="email"
+                color={"gray.500"}
+                _placeholder={{
+                  color: "gray.500",
+                }}
+              />
+              <Textarea
+                id="message"
+                placeholder="Message"
+                bg={"gray.100"}
+                border={0}
+                color={"gray.500"}
+                _placeholder={{
+                  color: "gray.500",
+                }}
+              />
+            </Stack>
+            <Button
+              fontFamily={"heading"}
+              mt={8}
+              w={"full"}
+              bgGradient="linear(to-r, blue.400,blue.600)"
+              color={"white"}
+              type="submit"
+              _hover={{
+                bgGradient: "linear(to-r, blue.400,blue.600)",
+                boxShadow: "xl",
+              }}
+            >
+              Submit
+            </Button>
+          </Box>
+          form
+        </Stack>
       </div>
-    </Curve>
+    </div>
   );
 };
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,10 +8,10 @@ import ProjectCard from "@/components/project-card";
 import Link from "next/link";
 import { Button } from "@chakra-ui/button";
 import { useRouter } from "next/navigation";
-import { Curve } from "@/components/transition";
 import kooperimage from "@/../public/images/kooper.webp";
 import aboutimage from "@/../public/images/about.webp";
 import { Tooltip } from "@chakra-ui/react";
+import LazyLoad from "react-lazyload";
 
 export default function Home() {
   const headerRef = useRef<HTMLDivElement>(null);
@@ -27,7 +27,7 @@ export default function Home() {
       setTimeout(() => {
         setIsHeaderInView(true);
         setHasAnimated(true);
-      }, 500)
+      }, 250);
     }
 
     const handleScroll = () => {
@@ -47,195 +47,209 @@ export default function Home() {
   }, [headerRef, hasAnimated]);
 
   return (
-    <Curve>
-      <div className="flex flex-col items-center justify-center">
-        <Image
-          className="top-0 left-0 absolute object-cover w-screen h-screen"
-          priority
-          draggable={false}
-          src="/images/banner.svg"
-          width={1920}
-          height={1080}
-          alt={"Banner Image"}
-        />
-        <KooperParticles />
-        <div className="container flex flex-col">
-          {/* Header */}
+    <div className="flex flex-col items-center justify-center">
+      <Image
+        className="top-0 left-0 absolute object-cover w-screen h-screen"
+        priority
+        draggable={false}
+        src="/images/banner.svg"
+        width={1920}
+        height={1080}
+        alt={"Banner Image"}
+      />
+      <KooperParticles />
+      <div className="container flex flex-col">
+        {/* Header */}
+        <div
+          id="header"
+          className="h-screen relative flex 2xl:flex-row flex-col-reverse 2xl:text-left text-center 2xl:justify-between justify-center items-center p-16"
+        >
           <div
-            id="header"
-            className="h-screen relative flex 2xl:flex-row flex-col-reverse 2xl:text-left text-center 2xl:justify-between justify-center items-center p-16"
+            className="mb-5 mouse-icon"
+            style={{
+              transform: isHeaderInView ? "none" : "translateY(25px)",
+              opacity: isHeaderInView ? 1 : 0,
+              transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s",
+            }}
           >
-            <div
-              className="mb-5 mouse-icon"
+            <div className="scroll" />
+          </div>
+          <div
+            style={{
+              transform: isHeaderInView ? "none" : "translateX(-200px)",
+              opacity: isHeaderInView ? 1 : 0,
+              transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s",
+            }}
+            ref={headerRef}
+            className="flex flex-col 2xl:gap-5 gap-0"
+          >
+            <p
+              className="white 2xl:text-9xl text-4xl"
               style={{
-                transform: isHeaderInView ? "none" : "translateY(25px)",
-                opacity: isHeaderInView ? 1 : 0,
-                transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s",
+                textShadow:
+                  "-0.5px -0.5px 0 #000, 0.5px -0.5px 0 #000, -0.5px 0.5px 0 #000, 0.5px 0.5px 0 #000",
               }}
             >
-              <div className="scroll" />
-            </div>
-            <div
+              I'm <b>Kooper</b>
+            </p>
+            <TypeAnimation
+              className="2xl:text-6xl text-2xl"
+              sequence={[
+                "Web Developer",
+                1000,
+                "App Developer",
+                1000,
+                "Game Developer",
+                1000,
+                "Software Engineer",
+                1000,
+              ]}
+              wrapper="span"
+              speed={75}
               style={{
-                transform: isHeaderInView ? "none" : "translateX(-200px)",
-                opacity: isHeaderInView ? 1 : 0,
-                transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s",
+                display: "inline-block",
+                textShadow:
+                  "-0.5px -0.5px 0 #000, 0.5px -0.5px 0 #000, -0.5px 0.5px 0 #000, 0.5px 0.5px 0 #000",
               }}
-              ref={headerRef}
-              className="flex flex-col 2xl:gap-5 gap-0"
-            >
-              <p
-                className="white 2xl:text-9xl text-4xl"
+              repeat={Infinity}
+            />
+          </div>
+          <LazyLoad
+            placeholder={
+              <div
                 style={{
-                  textShadow:
-                    "-0.5px -0.5px 0 #000, 0.5px -0.5px 0 #000, -0.5px 0.5px 0 #000, 0.5px 0.5px 0 #000",
+                  width: 400,
+                  height: 400,
+                  opacity: 0,
+                  filter: "blur(5px)",
                 }}
-              >
-                I'm <b>Kooper</b>
-              </p>
-              <TypeAnimation
-                className="2xl:text-6xl text-2xl"
-                sequence={[
-                  "Web Developer",
-                  1000,
-                  "App Developer",
-                  1000,
-                  "Game Developer",
-                  1000,
-                  "Software Engineer",
-                  1000,
-                ]}
-                wrapper="span"
-                speed={75}
-                style={{
-                  display: "inline-block",
-                  textShadow:
-                    "-0.5px -0.5px 0 #000, 0.5px -0.5px 0 #000, -0.5px 0.5px 0 #000, 0.5px 0.5px 0 #000",
-                }}
-                repeat={Infinity}
               />
-            </div>
+            }
+          >
             <Image
-              className="w-1/3"
+              priority
               style={{
                 opacity: isHeaderInView ? 1 : 0,
                 transform: isHeaderInView ? "none" : "translateX(200px)",
                 transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s",
                 filter: "drop-shadow(0px 2px 5px #222)",
+                width: 400,
+                height: 400,
               }}
               src={kooperimage}
               alt="Picture of Kooper"
               draggable={false}
-              width={540}
-              height={540}
+              width={400}
+              height={400}
             />
-          </div>
-          {/* About */}
-          <div
-            id="about"
-            className="flex 2xl:flex-row flex-col-reverse 2xl:text-left text-center items-center justify-center py-8 gap-10 2xl:px-0 px-6"
-          >
-            <Image
-              className="rounded-full lg:w-1/3 w-1/2"
-              src={aboutimage}
-              alt="Picture of Kooper"
-              loading="lazy"
-              draggable={false}
-              placeholder="blur"
-              width={407}
-              height={612}
-            />
-            <div className="flex flex-col gap-5">
-              <p className="text-black text-4xl font-bold">Who am I?</p>
-              <div className="flex flex-col gap-3">
-                <p className="text-black text-2xl">
-                  Hello! I'm Kooper, a developer based in{" "}
-                  <Tooltip label="Home to Janesville and Beloit, with a mix of urban areas, natural beauty like the Rock River, and over 250 miles of trails for outdoor enthusiasts.">
-                    <span className="text-main font-bold">Rock County, WI</span>
-                  </Tooltip>{" "}
-                  who's experienced in taking fullstack applications from
-                  scratch to production.
-                </p>
-                <p className="text-black text-2xl">
-                  I'm currently an incoming computer science student at{" "}
-                  <Tooltip label="A private, accredited university with over 200 online & on-campus programs. Known for affordability and focus on career-oriented degrees.">
-                    <span className="text-main font-bold">
-                      Southern New Hampshire University
-                    </span>
-                  </Tooltip>
-                  .
-                </p>
-                <p className="text-black text-2xl">
-                  I'm passionate about creating projects and{" "}
+          </LazyLoad>
+        </div>
+        {/* About */}
+        <div
+          id="about"
+          className="flex 2xl:flex-row flex-col-reverse 2xl:text-left text-center items-center justify-center py-8 gap-10 2xl:px-0 px-6"
+        >
+          <Image
+            className="rounded-full lg:w-1/3 w-1/2"
+            src={aboutimage}
+            alt="Picture of Kooper"
+            loading="lazy"
+            draggable={false}
+            placeholder="blur"
+            width={407}
+            height={612}
+          />
+          <div className="flex flex-col gap-5">
+            <p className="text-black text-4xl font-bold">Who am I?</p>
+            <div className="flex flex-col gap-3">
+              <p className="text-black text-2xl">
+                Hello! I'm Kooper, a developer based in{" "}
+                <Tooltip label="Home to Janesville and Beloit, with a mix of urban areas, natural beauty like the Rock River, and over 250 miles of trails for outdoor enthusiasts.">
+                  <span className="text-main font-bold">Rock County, WI</span>
+                </Tooltip>{" "}
+                who's experienced in taking fullstack applications from scratch
+                to production.
+              </p>
+              <p className="text-black text-2xl">
+                I'm currently an incoming computer science student at{" "}
+                <Tooltip label="A private, accredited university with over 200 online & on-campus programs. Known for affordability and focus on career-oriented degrees.">
                   <span className="text-main font-bold">
-                    learning new technologies
+                    Southern New Hampshire University
                   </span>
-                  . I'm always looking for new opportunities to{" "}
-                  <span className="text-main font-bold">collaborate</span> and{" "}
-                  create something amazing.
-                </p>
-              </div>
+                </Tooltip>
+                .
+              </p>
+              <p className="text-black text-2xl">
+                I'm passionate about creating projects and{" "}
+                <span className="text-main font-bold">
+                  learning new technologies
+                </span>
+                . I'm always looking for new opportunities to{" "}
+                <span className="text-main font-bold">collaborate</span> and{" "}
+                create something amazing.
+              </p>
             </div>
-          </div>
-          {/* Technologies */}
-          <div id="technologies" className="bg-full p-10 flex flex-col gap-3">
-            <p className="text-center text-black text-4xl font-bold">
-              Technologies
-            </p>
-            <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 2xl:grid-cols-8 gap-5 justify-center items-center">
-              {portfolioJSON.technologies.map((skill, index) => {
-                return (
-                  <Tooltip key={index} label={skill.description}>
-                    <div className="flex flex-row gap-3 bg-white p-3 rounded-lg shadow-md hover:drop-shadow-2xl">
-                      <Image
-                        src={skill.image}
-                        alt={skill.name}
-                        loading="lazy"
-                        draggable={false}
-                        width={24}
-                        height={24}
-                      />
-                      <p className="text-black text-xl">{skill.name}</p>
-                    </div>
-                  </Tooltip>
-                );
-              })}
-            </div>
-          </div>
-          {/* Projects */}
-          <div id="projects" className="p-10 flex flex-col gap-3">
-            <p className="text-center text-black text-4xl font-bold">
-              My Projects
-            </p>
-            <p className="text-center text-black text-3xl">
-              Check out my{" "}
-              <Link href="https://github.com/Kooperlol" target="_blank">
-                <span className="text-main">GitHub</span>
-              </Link>{" "}
-              for more!
-            </p>
-            <div className="flex lg:flex-row flex-col gap-5 p-5 justify-center lg:items-start items-center">
-              {portfolioJSON.projects.map((project, index) => {
-                return <ProjectCard key={index} {...project} />;
-              })}
-            </div>
-          </div>
-          {/* Contact */}
-          <div className="bg-full p-10 flex flex-col items-center gap-3">
-            <p className="text-center text-black text-4xl font-bold">
-              Let's get in touch!
-            </p>
-            <Button
-              backgroundColor={"#1463F3"}
-              color={"white"}
-              width={"min-content"}
-              onClick={() => router.push("/contact")}
-            >
-              Contact Me
-            </Button>
           </div>
         </div>
+        {/* Technologies */}
+        <div id="technologies" className="bg-full p-10 flex flex-col gap-3">
+          <p className="text-center text-black text-4xl font-bold">
+            Technologies
+          </p>
+          <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 2xl:grid-cols-8 gap-5 justify-center items-center">
+            {portfolioJSON.technologies.map((skill, index) => {
+              return (
+                <Tooltip key={index} label={skill.description}>
+                  <div className="flex flex-row gap-3 bg-white p-3 rounded-lg shadow-md hover:drop-shadow-2xl">
+                    <Image
+                      src={skill.image}
+                      alt={skill.name}
+                      loading="lazy"
+                      rel="dns-prefetch"
+                      draggable={false}
+                      width={24}
+                      height={24}
+                    />
+                    <p className="text-black text-xl">{skill.name}</p>
+                  </div>
+                </Tooltip>
+              );
+            })}
+          </div>
+        </div>
+        {/* Projects */}
+        <div id="projects" className="p-10 flex flex-col gap-3">
+          <p className="text-center text-black text-4xl font-bold">
+            My Projects
+          </p>
+          <p className="text-center text-black text-3xl">
+            Check out my{" "}
+            <Link href="https://github.com/Kooperlol" target="_blank">
+              <span className="text-main">GitHub</span>
+            </Link>{" "}
+            for more!
+          </p>
+          <div className="flex lg:flex-row flex-col gap-5 p-5 justify-center lg:items-start items-center">
+            {portfolioJSON.projects.map((project, index) => {
+              return <ProjectCard key={index} {...project} />;
+            })}
+          </div>
+        </div>
+        {/* Contact */}
+        <div className="bg-full p-10 flex flex-col items-center gap-3">
+          <p className="text-center text-black text-4xl font-bold">
+            Let's get in touch!
+          </p>
+          <Button
+            backgroundColor={"#1463F3"}
+            color={"white"}
+            width={"min-content"}
+            onClick={() => router.push("/contact")}
+          >
+            Contact Me
+          </Button>
+        </div>
       </div>
-    </Curve>
+    </div>
   );
 }
