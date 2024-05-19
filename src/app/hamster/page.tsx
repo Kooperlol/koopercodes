cb3e97a2ef388781227b3d2c764ab4341532fea7
Kooper Propp <kooperjack8@gmail.com>
2024-05-19 10:36:41 -0500

    Removed Mongo & Added more stats

--- src/app/hamster/page.tsx
+++ src/app/hamster/page.tsx
@@ -27,79 +27,66 @@ const HamsterPage = () => {
   );
   const [isLoading, setIsLoading] = useState(false);
   const [totalRotations, setTotalRotations] = useState<number>(0);
+  const [maxRotations, setMaxRotations] = useState<number>(0);
+  const [averageRotations, setAverageRotations] = useState<number>(0);
   const [innerX, setInnerX] = useState<number>(0);
 
-  interface RotationData {
-    history: {
-      timestamp: number;
-      value: {
-        timestamp: number;
-        value: number;
-      };
-    }[];
+  interface RotationDataEntry {
+    timestamp: number;
+    value: number;
   }
+  
+  interface RotationData {
+    history: RotationDataEntry[];
+  }
+  
+  const formatChartData = (data: RotationData): { name: string; value: number }[] => {
+    if (!data || !data.history) {
+      console.error("Invalid data format for chart. Missing required properties.");
+      return [];
+    }
+  
+    if (!Array.isArray(data.history) || data.history.length === 0) {
+      console.error("Invalid data format for chart. Empty history array.");
+      return [];
+    }
+  
+    const filteredAndSortedData = data.history
+      .filter((entry) => entry.value != null && typeof entry.timestamp === "number")
+      .sort((a, b) => a.timestamp - b.timestamp);
+  
+    return filteredAndSortedData.map((entry) => ({
+      name: new Date(entry.timestamp * 1000).toLocaleTimeString(),
+      value: entry.value,
+    }));
+  };
+  
 
   useEffect(() => {
-    const formatChartData = (
-      data: RotationData
-    ): { name: string; value: number }[] => {
-      if (!data || !data.history) {
-        console.error(
-          "Invalid data format for chart. Missing required properties."
-        );
-        return [];
-      }
-
-      if (!Array.isArray(data.history) || data.history.length === 0) {
-        console.error("Invalid data format for chart. Empty history array.");
-        return [];
-      }
-
-      let timestamps: (number | null)[] = data.history
-        .map((entry): number | null => {
-          if (!entry.value || typeof entry.value.timestamp !== "number") {
-            console.error(
-              "Invalid data format for chart. Missing timestamp in entry."
-            );
-            return null;
-          }
-          return entry.value?.timestamp;
-        })
-        .filter((timestamp) => timestamp !== null);
-
-      const filteredTimestamps = timestamps.filter(
-        (timestamp) => timestamp !== null
-      );
-
-      const values = data.history
-        .map((entry) => entry.value?.value)
-        .filter((value, index) => filteredTimestamps[index] !== null);
-
-      const sortedData = filteredTimestamps
-        .map((timestamp, index) => ({
-          timestamp,
-          value: values[index],
-        }))
-        .sort((a, b) => a.timestamp!! - b.timestamp!!);
-
-      return sortedData.map((entry) => ({
-        name: new Date(entry.timestamp!! * 1000).toLocaleTimeString(),
-        value: entry.value,
-      }));
-    };
-
     const fetchData = async () => {
       setIsLoading(true);
 
       try {
-        const apiUrl = "https://hamster-api.vercel.app/api/rotations";
-        const response = await axios.get(apiUrl);
-        const data = response.data;
-        const formattedData = formatChartData(data as RotationData);
+        const apiUrlHistory = "https://hamster-api.vercel.app/api/rotations";
+        const responseHistory = await axios.get(apiUrlHistory);
+        const dataHistory = responseHistory.data;
+        const formattedData = formatChartData(dataHistory as RotationData);
         setChartData(formattedData);
 
-        const rotations = await axios.get("/api/rotations");
-        setTotalRotations(rotations.data.data as number);
+        const apiUrlTotal = "https://hamster-api.vercel.app/api/total";
+        const responseTotal = await axios.get(apiUrlTotal);
+        const dataTotal = responseTotal.data.total_rotations;
+        setTotalRotations(dataTotal as number);
+
+        const apiUrlMax = "https://hamster-api.vercel.app/api/max";
+        const responseMax = await axios.get(apiUrlMax);
+        const dataMax = responseMax.data.max_rotation;
+        setMaxRotations(dataMax as number);
+
+        const apiUrlAverage = "https://hamster-api.vercel.app/api/average";
+        const responseAverage = await axios.get(apiUrlAverage);
+        const dataAverage = responseAverage.data.average_rotations;
+        setAverageRotations(dataAverage as number);
       } catch (error) {
         console.error("Error fetching data:", error);
       } finally {
@@ -154,6 +141,18 @@ const HamsterPage = () => {
                 <p>Total Miles:</p>
                 <Skeleton isLoaded={!isLoading}>
                   {(totalRotations * 0.00049589646).toFixed(5)}
+                </Skeleton>
+              </div>
+              <div className="flex flex-row gap-1">
+                <p>Average RPM:</p>
+                <Skeleton isLoaded={!isLoading}>
+                  {averageRotations.toLocaleString()}
+                </Skeleton>
+              </div>
+              <div className="flex flex-row gap-1">
+                <p>Record RPM:</p>
+                <Skeleton isLoaded={!isLoading}>
+                  {maxRotations.toLocaleString()}
                 </Skeleton>
               </div>
             </CardBody>
